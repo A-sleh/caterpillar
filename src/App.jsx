f6f0a9725c874dad087b1604b3c3acfbd7e1a5fc
A-sleh <businessabdo9@gmail.com>
2024-07-04 00:21:59 +0400

    updata all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 
-import Card from './components/Cards.jsx' ;
+
 import './assets/all.min.css'
-import {data} from './data.js' ;
+import React from 'react';
 
 export default function App() {
 
-    var dataShow = data.map((ele,index) => {
-        return <Card image = {ele.image} title = {ele.title} desc = {ele.desc} price = {ele.price} rate = {ele.rate} key={index} />
-    })
-
+    const [name,setName] = React.useState('Yes') ;
+    
+    function clickHandler() {
+        setName( preName => ( preName === 'Yes' ? 'No' : 'Yes') ) ;
+    }
     return(
-        <div style={{display : 'flex' , gap : '20px' , alignItems : 'center' , justifyContent: 'center' , margin : '40px' , flexWrap: 'wrap'}}>
-            {dataShow}
-        </div>
+        <p style={{fontSize: '30px' , textAlign: 'center' }} onClick={clickHandler}>{name}</p>
     )
 };
